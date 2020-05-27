ecb6689cc580c59987966d1983d80bb783876bbf
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-05-27 20:44:28 -0300

    Excluindo tarefa

--- JavaScript/Aplicação ToDoList/script.js
+++ JavaScript/Aplicação ToDoList/script.js
@@ -11,13 +11,27 @@ var todos = [
 function renderTodos(){
 
     listElement.innerHTML = '';
-    
+
     for (todo of todos){
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href','#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo('+ pos +')');
+
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
         listElement.appendChild(todoElement);
+
     }
 }
 
@@ -32,3 +46,8 @@ function addTodo(){
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos){
+    todos.splice(pos, 1);
+    renderTodos();
+}
