e804c850394b6119ad351b0fd5a9b9bd096c6767
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-05-27 20:27:45 -0300

    Criando novas tarefas

--- JavaScript/Aplicação ToDoList/script.js
+++ JavaScript/Aplicação ToDoList/script.js
@@ -9,6 +9,9 @@ var todos = [
 ];
 
 function renderTodos(){
+
+    listElement.innerHTML = '';
+    
     for (todo of todos){
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
@@ -19,3 +22,13 @@ function renderTodos(){
 }
 
 renderTodos();
+
+function addTodo(){
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+}
+
+buttonElement.onclick = addTodo;
