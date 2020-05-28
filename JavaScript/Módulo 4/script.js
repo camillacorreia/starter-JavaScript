108d661a491ace72adccb065d165a4fa937b21b6
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-05-27 21:47:39 -0300

    Promises

--- JavaScript/Módulo 4/script.js
+++ JavaScript/Módulo 4/script.js
@@ -1,10 +1,26 @@
-var xhr = new XMLHttpRequest();
+var myPromise = function() {
 
-xhr.open('GET','https://api.github.com/users/camillacorreia');
-xhr.send(null);
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/camillacorreia');
+        xhr.send(null);
 
-xhr.onreadystatechange = function(){
-    if (xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText));
-    }
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4){
+                if(xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                }else{
+                    reject('Erro na requisição');
+                }
+            }
+        }
+    });
 }
+
+myPromise()
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.warn(error);
+    });
