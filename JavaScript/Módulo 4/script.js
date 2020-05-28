83d81f1f20d0ca7e529209cbe6e05d518dc26f36
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-05-27 22:05:53 -0300

    Utilizando axios

--- JavaScript/Módulo 4/script.js
+++ JavaScript/Módulo 4/script.js
@@ -1,23 +1,4 @@
-var myPromise = function() {
-
-    return new Promise(function(resolve, reject){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/camillacorreia');
-        xhr.send(null);
-
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4){
-                if(xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                }else{
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
-
-myPromise()
+axios.get('https://api.github.com/users/camillacorreia')
     .then(function(response) {
         console.log(response);
     })
