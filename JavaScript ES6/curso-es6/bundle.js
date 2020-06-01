cb2df7053b88676ad007e26f774c85d27431befa
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-01 20:02:57 -0300

    Template Literals
    Incluir variáveis dentro de strings

---
+++ JavaScript ES6/curso-es6/bundle.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var nome = "Rafael";
+var idade = 13; //console.log('Meu nome é ' + nome +' e tenho ' + idade + ' anos ');
+
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos."));
--- JavaScript ES6/curso-es6/main.js
+++ JavaScript ES6/curso-es6/main.js
@@ -1,52 +1,6 @@
-// REST
+const nome = "Rafael";
+const idade = 13;
 
-const usuario = {
-    nome: 'Camilla',
-    idade: 19,
-    empresa: 'Geek Business Lab'
-};
+//console.log('Meu nome é ' + nome +' e tenho ' + idade + ' anos ');
 
-const { nome, ...resto } = usuario;
-
-console.log(nome);
-console.log(resto);
-
-const arr = [1,2,3,4];
-
-const[ a, b, ...c ] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-function soma(...params){
-    return params.reduce((total, next) => total + next);
-}
-
-console.log(soma(a, b, ...params));
-
-function soma(...params){
-    return params;
-}
-
-console.log(soma(1,2,3,4,5,6,7,8,9,10));
-
-
-// SPREAD
-
-const arr1  = [1,2,3];
-const arr2 = [4,5,6];
-
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
-
-const usuario1 = {
-    nome: 'Nandes',
-    idade: 23,
-    empresa: 'Unilever',
-};
-
-const usuario2 = { ...usuario1, empresa: 'Google'};
-
-console.log(usuario2);
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
