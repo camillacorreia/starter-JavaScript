8ce1e93b95b877c367c85771269f7d206498c02a
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-01 17:34:45 -0300

    Classes

--- JavaScript ES6/curso-es6/main.js
+++ JavaScript ES6/curso-es6/main.js
@@ -1,9 +1,30 @@
-class Teste{
-    metodo(){
-
+class List{
+    constructor(){
+        this.data = [];
     }
 
-    outro(){
-
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List{
+    constructor(){
+        super();
+
+        this.usuario = 'Camilla';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
