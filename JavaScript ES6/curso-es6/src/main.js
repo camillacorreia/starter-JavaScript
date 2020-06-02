ef31d3cd6977cbb2ebfccb036a3433b313ce0cda
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-02 10:52:51 -0300

    Async/await

--- JavaScript ES6/curso-es6/src/main.js
+++ JavaScript ES6/curso-es6/src/main.js
@@ -1,1 +1,23 @@
-alert("Ok!");
+const minhaPromise = () => new Promise((resolve, reject) =>
+    setTimeout(() => { resolve('OK') }, 2000));
+
+// minhaPromise().then(response => {
+//    console.log(response);
+// });
+
+//async function executaPromise() {
+//    console.log(await minhaPromise());
+//    console.log(await minhaPromise());
+//    console.log(await minhaPromise());
+//}
+
+
+const executaPromise = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+};
+
+executaPromise();
