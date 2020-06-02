f7fcb707e5e27dc7b47540fceadf9864b037df19
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-01 23:55:29 -0300

    Módulo 2

---
+++ JavaScript ES6/Módulo 2/main.js
@@ -0,0 +1,12 @@
+//1.1
+import ClasseUsuario from './functions';
+
+ClasseUsuario.info();
+
+//1.2
+
+import { idade } from './functions';
+console.log(idade);
+
+//1.3
+import defaultUsuario, { idade as IdadeUsuario } from './functions';
