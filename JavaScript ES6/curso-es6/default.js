e0d1822a6bc2e665158e215a5f69fe5fd9ece46c
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-01 23:48:18 -0300

    Webpack dev server

--- JavaScript ES6/curso-es6/default.js
+++
@@ -1,3 +0,0 @@
-export default function somando(a,b) {
-    return a + b;
-}
--- JavaScript ES6/curso-es6/webpack.config.js
+++ JavaScript ES6/curso-es6/webpack.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-    entry: './main.js',
+    entry: './src/main.js',
     output: {
-        path: __dirname,
+        path: __dirname + '/public',
         filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase:__dirname + '/public'
     },
     module: {
         rules:[
