b0b14d895afd657aa56c629d22eb419557eb58c1
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-03 20:54:10 -0300

    CRUD

--- node-api/server.js
+++ node-api/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 // Iniciando o DB
-mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 requireDir('./src/models');
 
 // Rotas
