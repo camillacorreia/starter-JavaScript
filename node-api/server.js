f3e88e37785315a6778540c683690f5c7a6e1b38
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-03 17:16:26 -0300

    Conectando Database

--- node-api/server.js
+++ node-api/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/node-api', { useNewUrlParser:true });
+
+// Primeira rota
 app.get('/', (req, res) => {
     res.send("Hello, Camilla!");
 });
