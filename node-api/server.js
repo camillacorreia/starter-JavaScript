4e1c53d9cdc91ac2db4957fc72abd59c24ee05d5
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-03 17:55:51 -0300

    Criando model de produto

--- node-api/server.js
+++ node-api/server.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir =require('require-dir');
 
 // Iniciando o App
 const app = express();
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/node-api', { useNewUrlParser:true });
+mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true, useUnifiedTopology: true })
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // Primeira rota
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+
     res.send("Hello, Camilla!");
 });
 
