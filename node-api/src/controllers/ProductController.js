6b2d1f025cc7090a77eb58cfcc4e0273610612c8
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-03 21:11:33 -0300

    Paginação da lista

--- node-api/src/controllers/ProductController.js
+++ node-api/src/controllers/ProductController.js
@@ -4,7 +4,8 @@ const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res) {
-        const products = await Product.find();
+        const { page = 1} =  req.query;
+        const products = await Product.paginate({}, { page, limit: 10 });
 
         return res.json(products);
     },
