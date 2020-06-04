05242afd5a2146521b604ad0ef16f383b600a947
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-03 21:27:41 -0300

    Adicionando CORS
    Permitir acesso externo a nossa API

--- node-api/server.js
+++ node-api/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Iniciando o DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
