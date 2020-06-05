4c7661aa592f8374f9a86c7aacd0119bb09d395b
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-05 16:31:21 -0300

    Armazenando no estado

--- ReactJS/huntweb/src/pages/main/index.js
+++ ReactJS/huntweb/src/pages/main/index.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+    state = {
+        products: [],
+    }
     componentDidMount() {
         this.loadProducts();
     }
     loadProducts = async () => {
         const response = await api.get('/products');
 
-        console.log(response.data.docs);
+        this.setState({ products: response.data.docs });
     }
     render() {
-        return <h1>Hello, Camilla!</h1>
+        return (
+            <div className="product-list">
+              {this.state.products.map(product => (
+                  <h2> key={product._id}>{product.title}</h2>
+              ))}  
+            </div>
+        )
     }
 }
