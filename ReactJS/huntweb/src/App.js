fee61c88bee16fef50b46eddd9e3f54885f35ddd
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-08 18:30:09 -0300

    Configurando navegação

--- ReactJS/huntweb/src/App.js
+++ ReactJS/huntweb/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Routes from "./routes";
 
 import './styles.css';
 
@@ -8,7 +9,7 @@ import Main from './pages/main';
 const App = () => (
   <div className="App">
     <Header/>
-    <Main/>
+    <Routes/>
   </div>
 );
 
--- ReactJS/huntweb/src/pages/main/index.js
+++ ReactJS/huntweb/src/pages/main/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
 import './styles.css';
 
 export default class Main extends Component {
@@ -50,7 +52,8 @@ export default class Main extends Component {
                     <article key={product._id}>
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
-                        <a href="">Acessar</a>
+
+                        <Link to={`/products/${product._id}`}>Acessar</Link>
                     </article>
                 ))}  
             
